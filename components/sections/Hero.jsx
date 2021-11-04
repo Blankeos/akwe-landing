7ab19f9f9791b8cfb3eb5b96d2dc930faa87e59e
Blankeos <carloantonioct@gmail.com>
2021-11-04 17:17:50 +0800

    Added About page and cleaned up Hero

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -4,8 +4,11 @@ function Hero() {
   return (
     <div className="max-w-5xl grid gap-10 md:gap-0 md:grid-cols-[2fr,1fr] mx-auto py-28 px-10 grid-cols-1 overflow-hidden">
       <div className="flex flex-col space-y-5 h-full justify-center">
-        <h1 className="font-black tracking-tighter text-5xl md:text-7xl text-transparent bg-clip-text bg-gradient-to-br from-[#feabf1] to-[#033f8f] filter drop-shadow-sm">
-          CYBERGENCE 2021
+        <h1 className="flex flex-col">
+          <span className="uppercase text-gray-400">Acquaintance Party</span>
+          <span className="font-black tracking-tighter text-5xl md:text-7xl text-transparent bg-clip-text bg-gradient-to-br from-[#feabf1] to-[#033f8f] filter drop-shadow-sm">
+            CYBERGENCE 2021
+          </span>
         </h1>
         <p className="max-w-sm text-lg font-medium text-gray-700">
           Emerging CICTzens into a Virtual Iridescent World.
@@ -13,6 +16,14 @@ function Hero() {
         <div className="font-light text text-gray-600">
           <p>November 12, 2021, FRIDAY</p>
           <p>9:00 AM - 11:30 AM, 1:30 PM - 3:30 PM</p>
+        </div>
+        <div className="flex space-x-5">
+          <button className="px-4 py-1 border border-blue-400">
+            Search Students
+          </button>
+          <button className="px-4 py-1 border border-blue-400 bg-blue-400">
+            See Events
+          </button>
         </div>
       </div>
       <div className="flex items-center justify-center">
