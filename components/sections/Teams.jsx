fe4c603efd970a7e912a11d14c90fccd03cbaa4c
ashcbrd <cabardoash@gmail.com>
2021-11-05 07:29:44 +0800

    removed hover effect in mobile view

--- components/sections/Teams.jsx
+++ components/sections/Teams.jsx
@@ -20,12 +20,12 @@ export const Teams = ({ data }) => {
       <h2 className="text-center tracking-tighter font-extrabold text-5xl mt-20 text-gray-800">
         Meet the Teams
       </h2>
-      <div className="flex flex-wrap justify-center w-full mx-auto my-10 hover: overflow-hidden">
+      <div className="flex flex-wrap justify-center w-full mx-auto my-10 overflow-hidden">
         {data.teams.map((team) => {
           return (
             <Link key={team.teamID} href={`/teams/${team.slug}`}>
               <a>
-                <div className="cursor-pointer flex flex-col w-80 h-80 justify-end text-center shadow-lg border border-gray-200 m-5 rounded-xl hover:scale-110 hover:bg-gray-100 hover:border-blue-500 hover:shadow-xl duration-100">
+                <div className="cursor-pointer flex flex-col w-80 h-80 justify-end text-center shadow-lg border border-gray-200 m-5 rounded-xl md:hover:scale-110 hover:bg-gray-100 hover:border-blue-500 hover:shadow-xl duration-100">
                   <span className="flex-grow flex items-center justify-center text-8xl">
                     {emojiDict[team.teamID]}
                   </span>
