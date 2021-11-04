202a54e06641d0add81b63adf300652fc529286d
Blankeos <carloantonioct@gmail.com>
2021-11-04 14:18:52 +0800

    Installed Tailwind + Site structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex flex-col min-h-screen">
+      <nav className="h-20 bg-red-200 sticky top-0">NAV</nav>
+      <Component {...pageProps} />
+      <footer className="h-20 bg-indigo-500"></footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
