05ef6daec5a61c21ae254ed90ace3be4ae3045c3
ashcbrd <cabardoash@gmail.com>
2021-11-04 23:01:22 +0800

    made the footer mobile responsive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,22 +3,24 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="bg-gradient-to-br from-[#feabf1] to-[#033f8f]  z-40 w-full pt-20 pb-36 px-10 sticky top-0 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-80 border border-gray-200">
-      <div className="max-w-5xl w-full mx-auto grid grid-cols-[2fr,1fr] h-full">
+    <footer className="bg-gradient-to-br from-[#feabf1] to-[#033f8f]  z-40 w-full pt-16 md:pt-20 pb-16 md:pb-20 px-10 sticky top-0 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-80 border border-gray-200">
+      <div className="flex items-center justify-center flex-col jus max-w-5xl w-full mx-auto md:grid grid-cols-[2fr,1fr] h-full">
         <div className="flex space-x-5">
-          <div className="h-36 w-36 rounded-full bg-gray-700 flex justify-center items-center">
+          <div className="hidden h-36 w-36 rounded-full bg-gray-700 md:flex justify-center items-center">
             <img
               className="w-32 h-32"
               src="https://raw.githubusercontent.com/wvsu-cict-code/cict-logo/ffc1f42e6fa6e04c33df791495ec4ef04c3f4d84/mascot.svg"
             />
             ¸
           </div>
-          <div className="flex flex-col space-y-2">
-            <p className="text-white text-lg font-bold max-w-sm">
+          <div className="flex md:justify-center w-full md:text-left mb-5 text-center md:text-start flex flex-col space-y-2">
+            <p className="text-white md:text-lg font-bold max-w-sm">
               CICT VIRTUAL ACQUAINTANCE PARTY
             </p>
-            <p className="text-gray-200">Event Landing Website</p>
-            <p className="text-gray-200">
+            <p className="text-xs md:text-base text-gray-200">
+              Event Landing Website
+            </p>
+            <p className="text-xs md:text-base text-gray-200">
               Made with 💖 by{" "}
               <Link href="https://github.com/keiruu">
                 <a className="text-white hover:text-pink-300" target="_blank">
@@ -39,12 +41,9 @@ function Footer() {
               </Link>
               .
             </p>
-            <p className="text-gray-200 text-xs pt-5">
-              © 2021 WVSU-CICT. All Rights Reserved.
-            </p>
           </div>
         </div>
-        <div className="flex flex-col text-sm space-y-1.5 text-gray-100">
+        <div className="flex md:justify-center w-full md:text-left text-center mt-2 flex md:flex-col text-xs md:text-sm space-y-1.5 text-gray-100">
           <a>🐦 Just a Birb Not a Link</a>
           <Link href="https://github.com/blankeos/akwe-landing">
             <a className="hover:text-pink-300">👾 GitHub Source Code</a>
@@ -54,6 +53,9 @@ function Footer() {
           </Link>
         </div>
       </div>
+      <p className="text-gray-200 md:w-full text-xs text-center pt-5">
+        © 2021 WVSU-CICT. All Rights Reserved.
+      </p>
     </footer>
   );
 }
